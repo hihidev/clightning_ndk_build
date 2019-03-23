b29f4730df548bdd01e3a91abaac1e7692af1066
R <r@hihi.dev>
2019-03-23 00:11:28 +0000

    Update c-lightning to v0.7.0

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export REPO=https://github.com/ElementsProject/lightning.git
-export COMMIT=21afe1c0f403b470f5ef6f117f9e3e58d48b7935
+export COMMIT=4a11bc07f9bc474cbe5eb6f9e45c71075d40fda3
 export TARGETHOST=armv7a-linux-androideabi
 export BITS=32
 
